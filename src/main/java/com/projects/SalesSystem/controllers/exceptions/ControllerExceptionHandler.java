134865d82145b3a2a89334c923abb96e67a6c7ac
Felipe Milana <felipe.milana97@gmail.com>
2021-05-18 14:21:42 -0300

    Handling exceptions

---
+++ src/main/java/com/projects/SalesSystem/controllers/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.projects.SalesSystem.controllers.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.projects.SalesSystem.services.exceptions.DataIntegrity;
+import com.projects.SalesSystem.services.exceptions.ObjectNotFound;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFound.class)
+	 public ResponseEntity<StandardError> objectNotFound(ObjectNotFound e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), 
+				status.getReasonPhrase(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	 }
+	
+	@ExceptionHandler(DataIntegrity.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrity e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), 
+				status.getReasonPhrase(), e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err = new ValidationError(System.currentTimeMillis(), status.value(),
+				status.getReasonPhrase(), "Validation error", request.getRequestURI());
+		
+		for(FieldError f: e.getBindingResult().getFieldErrors()) {
+			err.getErrors().add(new FieldMessage(f.getField(), f.getDefaultMessage()));
+		}
+		
+		return ResponseEntity.status(status).body(err);
+	}
+}

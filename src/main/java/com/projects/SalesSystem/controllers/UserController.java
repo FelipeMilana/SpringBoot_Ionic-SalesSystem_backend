6f7e64770465c48f386423f052e034a6ed2ecc15
Felipe Milana <felipe.milana97@gmail.com>
2021-05-25 18:49:19 -0300

    UserController

---
+++ src/main/java/com/projects/SalesSystem/controllers/UserController.java
@@ -0,0 +1,53 @@
+package com.projects.SalesSystem.controllers;
+
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.projects.SalesSystem.entities.dto.UserDTO;
+import com.projects.SalesSystem.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
+		UserDTO user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
+	}
+	
+	@GetMapping(value = "/email")
+	public ResponseEntity<UserDTO> findById(@RequestParam(value = "value") String email) {
+		UserDTO user = userService.findByEmail(email);
+		return ResponseEntity.ok().body(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody UserDTO objDTO) {
+		UserDTO user = userService.insertFromDTO(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@Valid @RequestBody UserDTO objDTO, @PathVariable Long id) {
+		userService.update(id, objDTO);
+		return ResponseEntity.noContent().build();
+	}
+}

e3201900d374e3702e077e35b7578a109943e65e
Felipe Milana <felipe.milana97@gmail.com>
2021-08-12 15:42:00 -0300

    TestConfig

--- src/main/java/com/projects/SalesSystem/SalesSystemApplication.java
+++ src/main/java/com/projects/SalesSystem/SalesSystemApplication.java
@@ -1,17 +1,11 @@
 package com.projects.SalesSystem;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.projects.SalesSystem.services.DBService;
-
 @SpringBootApplication
 public class SalesSystemApplication implements CommandLineRunner{
-
-	@Autowired
-	private DBService dbService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SalesSystemApplication.class, args);
@@ -19,7 +13,6 @@ public class SalesSystemApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		dbService.instantiateDatabase();
 	}
 
 }
